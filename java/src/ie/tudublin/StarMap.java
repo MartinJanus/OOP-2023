3c858d81ebb4fb12ca328fadfeca9402f5fcb0cf
Martin Januska <martinasjanuska@gmail.com>
2023-02-13 17:52:36 +0000

    Grid Part Fin

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -11,15 +11,31 @@ public class StarMap extends PApplet
 
 	public void setup() {
 		colorMode(HSB);
-		background(0);
+		background(1);
 
 		smooth();
 		
 	}
 
+	public void drawGrid()
+	{
+		float border = 50.0f; 
+		int count = 10; 
+		for(int i = 0; i<=count; i++)
+		{	
+			stroke(102,255,153);
+			fill(102,255,153);
+			float gap = ((width - (border * 2.0f)) / count);
+			float x = border + (gap * i);
+			line(x, border , x , height - border);
+			line(border, x , width - border, x);
+		}
+	}
 		
 	public void draw()
 	{	
 		strokeWeight(2);		
+		drawGrid();
 	}
+
 }
