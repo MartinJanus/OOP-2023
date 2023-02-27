f4d6f0f9a3193f73d234b1be5302319d8db2957f
Martin Januska <martinasjanuska@gmail.com>
2023-02-27 19:22:53 +0000

    more audio

--- java/src/ie/tudublin/Audio2.java
+++ java/src/ie/tudublin/Audio2.java
@@ -4,6 +4,7 @@ import processing.core.PApplet;
 import ddf.minim.AudioBuffer;
 import ddf.minim.AudioInput;
 import ddf.minim.Minim;
+import ddf.minim.analysis.FFT;
 import ddf.minim.AudioPlayer;
 
 public class Audio2 extends PApplet 
@@ -13,6 +14,8 @@ public class Audio2 extends PApplet
     AudioInput ai;
     AudioPlayer ap;
     AudioBuffer ab;
+
+    FFT fft; 
 
 
     public void settings()
@@ -26,9 +29,12 @@ public class Audio2 extends PApplet
         ai = m.getLineIn(Minim.MONO, width, 44100, 16);
         ab = ai.mix; 
         lerpedBuffer = new float[width];
+
+        fft = new FFT(width, 44100);
     }
 
     float[] lerpedBuffer;
+
 
     public void draw()
     {
@@ -39,10 +45,33 @@ public class Audio2 extends PApplet
         for(int i = 0; i < ab.size(); i++ )
         {
             stroke(map(i,0,ab.size(), 0,255),255,255);
-            lerpedBuffer[i] = lerp(lerpedBuffer[i], ab.get(i), 0.1f); // 2.0f for more delay latency 
-            float f = abs(lerpedBuffer[i] * half);
+            lerpedBuffer[i] = lerp(lerpedBuffer[i], ab.get(i), 0.05f); // 2.0f for more delay latency 
+            float f = abs(lerpedBuffer[i] * half * 2.0f);
             line(i, half + f, i, half - f);
         }
+
+        fft.forward(ab);
+        stroke(255);
+
+        int highestIndex = 0; 
+
+        for(int i = 0; i < fft.specSize() / 2; i++)
+        {
+            line(i * 2.0f , height, i * 2.0f, height - fft.getBand(i) * 5.0f);
+            if(fft.getBand(i) > fft.getBand(highestIndex))
+            {
+                highestIndex = i;
+            }
+        }
+
+        float freq = fft.indexToFreq(highestIndex);
+        fill(255);
+        text("Freq: " + freq, 10, 50);
+
+        float y = map(freq, 1000.0f, 2500.0f, height, 0);
+        lerpedY = lerp(lerpedY, y, 0.1f);
+        circle(200, y, 50);
+
 
         // println(map(5,0,10,1000,2000));
         // println(map1(5,2,10,1000,2000));
