bd4b9aa871578b046471514344f3b3cda51eab19
Martin Januska <martinasjanuska@gmail.com>
2023-03-06 15:09:18 +0000

    Working Game of Life

--- java/src/ie/tudublin/Life.java
+++ java/src/ie/tudublin/Life.java
@@ -5,23 +5,26 @@ import processing.core.PApplet;
 public class Life extends PApplet
 {
 
+	LifeBoard board; 
+
 	public void settings()
 	{
-		size(500, 500);
+		size(1000, 1000);
 	}
 
 	public void setup() {
-		colorMode(HSB);
+		colorMode(RGB);
 		background(0);
-
-		
-		
+		board = new LifeBoard(500, this);		
+		board.randomise();
 	}
 
 	
 	
 	public void draw()
 	{	
-		
+		background(0);
+		board.render();
+		board.applyRules();
 	}
 }
